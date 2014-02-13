ec1b40fc60447ab9db415a64053167c94baa0f8b
vagrant <vagrant@precise32.(none)>
2014-02-13 00:52:46 +0000

    ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
 }
 
@@ -18,16 +17,10 @@ function initializePage() {
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
 	e.preventDefault();
-
-	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
 	console.log("User clicked on project " + idNumber);
-
 	var url = "/project/" + idNumber;
 	$.get(url, addProject);
 }
@@ -36,17 +29,11 @@ function addProject(result) {
 	console.log(result);
 	var idNumber = result.id;
 	var detailsSelector = "div#project" + idNumber + " div.details";
-
 	var htmlInsert = 
 		'<img src="' + result.image + '" class="detailsImage">' +
 		'<h3>' + result.title + '</h3>' +
 		'<p><small>' + result.date + '</small></p>' +
 		'<p>' + result.summary + '</p>';
-
-	//result.id. result.title, result.date, result.summary, result.image
-	//include image, small header with date, and summary
-	//Give the image the class detailsImage
-
 	$(detailsSelector).html(htmlInsert);
 }
 
@@ -63,7 +50,6 @@ function randomizeColors(e) {
 function applyPalette(result) {
 	console.log(result);
 	var colors = result.colors.hex;
-
 	$('body').css('background-color', colors[0]);
 	$('.thumbnail').css('background-color', colors[1]);
 	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
--- routes/palette.js
+++ routes/palette.js
@@ -1,8 +1,5 @@
 var palettes = require('../palettes.json');
-
 exports.randomPalette = function(req, res) {
-	// get a random palette from the top ones
 	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.json(randomPalette);
-	// res.send('Your random palette is called: ' + randomPalette['title']);
+	res.json(randomPalette); // pass palette via JSON object it should return 
 }
